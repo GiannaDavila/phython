ea24110e9b5ecdbcf2c14ccd6ea6099e77cd4c44
GiannaDavila <giannagiannadavila@gmail.com>
2022-11-16 13:19:35 -0500

    first python class

---
+++ binary-search.py
@@ -0,0 +1,19 @@
+
+from typing import List
+
+def search (nums: List[int], target: int) -> int:
+    length = len(nums) -1
+    low, high = length
+
+    while low <= high:
+        mid = (low + high) // 2 # 3
+        
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            high = mid - 1 
+        else: 
+            low = mid + 1 
+    return -1 
+
+print (search([1,7,10,15,30,40], 7))
